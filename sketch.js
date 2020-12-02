e1b084feb51641cde75708fff540063c6371161c
NehaArora321 <70069247+NehaArora321@users.noreply.github.com>
2020-12-02 21:50:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -167,7 +167,7 @@ function keyPressed(){
 }
 
 async function getBackColor(){
-  var response= await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var response= await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
   var responseJSON= await response.json();
   var dt=responseJSON.datetime;
   var hour=dt.slice(11,13);
